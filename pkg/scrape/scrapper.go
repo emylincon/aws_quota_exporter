b7a9f97fa49b2714ab1ec5b293dbe090f7810eb0
Emeka E Ugwuanyi <emylincon@gmail.com>
2023-03-27 23:00:04 +0100

    yaml config parser

--- pkg/scrape/scrapper.go
+++ pkg/scrape/scrapper.go
@@ -28,21 +28,24 @@ func NewScraper(profileName string) (*Scraper, error) {
 }
 
 // CreateScraper Scrape Quotas from AWS
-func (s *Scraper) CreateScraper(region, serviceCode string) func() ([]*pkg.PrometheusMetric, error) {
+func (s *Scraper) CreateScraper(regions []string, serviceCode string) func() ([]*pkg.PrometheusMetric, error) {
 	return func() ([]*pkg.PrometheusMetric, error) {
 		ctx := context.Background()
 
 		sclient := sq.NewFromConfig(s.cfg)
 		input := sq.ListServiceQuotasInput{ServiceCode: &serviceCode}
+		metricList := []*pkg.PrometheusMetric{}
 
-		opts := func(o *sq.Options) { o.Region = region }
-
-		r, err := sclient.ListServiceQuotas(ctx, &input, opts)
-		if err != nil {
-			fmt.Printf("Failed to get service quotas: %v", err)
-			return nil, err
+		for _, region := range regions {
+			metrics, err := getServiceQuotas(ctx, region, &input, sclient)
+			if err != nil {
+				fmt.Printf("Failed to get service quotas: %v", err)
+				return nil, err // TODO: return errors
+			}
+			metricList = append(metricList, metrics...)
 		}
-		return Transform(r, region)
+		return metricList, nil
+
 	}
 
 }
@@ -64,4 +67,15 @@ func Transform(quotas *sq.ListServiceQuotasOutput, region string) ([]*pkg.Promet
 
 func createMetricName(serviceCode, quotaName string) string {
 	return fmt.Sprintf("aws_quota_%s_%s", serviceCode, pkg.PromString(quotaName))
+}
+
+func getServiceQuotas(ctx context.Context, region string, sqInput *sq.ListServiceQuotasInput, client *sq.Client) ([]*pkg.PrometheusMetric, error) {
+	opts := func(o *sq.Options) { o.Region = region }
+
+	r, err := client.ListServiceQuotas(ctx, sqInput, opts)
+	if err != nil {
+		fmt.Printf("Failed to get service quotas: %v", err)
+		return nil, err
+	}
+	return Transform(r, region)
 }
